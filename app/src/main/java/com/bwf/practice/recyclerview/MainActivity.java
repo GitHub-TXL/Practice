c4aa1eaeb28aa2d21b3fe6639dc6cdf79abd76a5
Practice <243919247@qq.com>
2017-01-18 10:54:32 +0800

    写轮眼

--- app/src/main/java/com/bwf/practice/recyclerview/MainActivity.java
+++ app/src/main/java/com/bwf/practice/recyclerview/MainActivity.java
@@ -1,11 +1,21 @@
 package com.bwf.practice.recyclerview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
 
+import com.bwf.practice.VideoPlayActivity;
+import com.bwf.practice.eyes.AndroidGifActivity;
 import com.bwf.practice.R;
+import com.bwf.practice.eyes.BanL;
+import com.bwf.practice.eyes.BanX;
+import com.bwf.practice.eyes.KaKaXi;
+import com.bwf.practice.eyes.Quan;
+import com.bwf.practice.eyes.Zhi;
+import com.bwf.practice.eyes.Zuo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,32 +24,74 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private List<Product> products;
     private Product product;
-    private int[] images={R.mipmap.a1,R.mipmap.a2,R.mipmap.a3,R.mipmap.a4,R.mipmap.a5,R.mipmap.a6,R.mipmap.a7,R.mipmap.a8,R.mipmap.a9,R.mipmap.a10,R.mipmap.a11};
+    private int[] images = {R.mipmap.a1, R.mipmap.a2, R.mipmap.a3, R.mipmap.a4,
+            R.mipmap.a5, R.mipmap.a6, R.mipmap.a7, R.mipmap.a8,
+            R.mipmap.a9, R.mipmap.a10, R.mipmap.a11,
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView= (RecyclerView) findViewById(R.id.recycler);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
         //设置layoutManager
-        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         //设置adapter
         initData();
-        RecyclerViewAdapter adapter=new RecyclerViewAdapter(products);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(products);
         recyclerView.setAdapter(adapter);
         //设置item之间的间隔
-        SpacesItemDecoration decoration=new SpacesItemDecoration(16);
+        SpacesItemDecoration decoration = new SpacesItemDecoration(16);
         recyclerView.addItemDecoration(decoration);
+        adapter.setOnItemClickListener(new RecyclerViewAdapter.OnRecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View view, Product data) {
+                switch (data.getImg()) {
+                    case R.mipmap.a1:
+                        startActivity(new Intent(MainActivity.this, AndroidGifActivity.class));
+                        break;
+                    case R.mipmap.a2:
+                        startActivity(new Intent(MainActivity.this, BanL.class));
+                        break;
+                    case R.mipmap.a3:
+                        startActivity(new Intent(MainActivity.this, BanX.class));
+                        break;
+                    case R.mipmap.a4:
+                        startActivity(new Intent(MainActivity.this, KaKaXi.class));
+                        break;
+                    case R.mipmap.a5:
+                        startActivity(new Intent(MainActivity.this, Quan.class));
+                        break;
+                    case R.mipmap.a6:
+                        startActivity(new Intent(MainActivity.this, Zhi.class));
+                        break;
+                    case R.mipmap.a7:
+                        startActivity(new Intent(MainActivity.this, Zuo.class));
+                        break;
+                    case R.mipmap.a8:
+                        startActivity(new Intent(MainActivity.this, VideoPlayActivity.class));
+                        break;
+                    case R.mipmap.a9:
+                        startActivity(new Intent(MainActivity.this, VideoPlayActivity.class));
+                        break;
+                    case R.mipmap.a10:
+                        startActivity(new Intent(MainActivity.this, VideoPlayActivity.class));
+                        break;
+                    case R.mipmap.a11:
+                        startActivity(new Intent(MainActivity.this, VideoPlayActivity.class));
+                        break;
+                }
+
+            }
+        });
 
     }
-    private void initData(){
-        products=new ArrayList<>();
-      for(int i=0;i<images.length;i++){
-            product=new Product(images[i],"拉克丝"+i);
+
+    private void initData() {
+        products = new ArrayList<>();
+        for (int i = 0; i < images.length; i++) {
+            product = new Product(images[i], "拉克丝" + i);
             products.add(product);
         }
-
-
-
-
     }
 }
--- app/src/main/java/com/bwf/practice/recyclerview/RecyclerViewAdapter.java
+++ app/src/main/java/com/bwf/practice/recyclerview/RecyclerViewAdapter.java
@@ -15,17 +15,36 @@ import java.util.List;
  * Created by Administrator on 2016/12/29.
  */
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MasonryView>{
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MasonryView> implements View.OnClickListener {
     private List<Product> products;
 
+    @Override
+    public void onClick(View v) {
+        if (mOnItemClickListener != null) {
+            //注意这里使用getTag方法获取数据
+            mOnItemClickListener.onItemClick(v, (Product) v.getTag());
+        }
+    }
+
+    public static interface OnRecyclerViewItemClickListener {
+        void onItemClick(View view, Product data);
+    }
+
+    private OnRecyclerViewItemClickListener mOnItemClickListener = null;
+
+    public void setOnItemClickListener(OnRecyclerViewItemClickListener listener) {
+        this.mOnItemClickListener = listener;
+    }
 
     public RecyclerViewAdapter(List<Product> list) {
-        products=list;
+        products = list;
     }
 
     @Override
     public MasonryView onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.waterfall_item, viewGroup, false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.waterfall_item, viewGroup, false);
+        //将创建的View注册点击事件
+        view.setOnClickListener(this);
         return new MasonryView(view);
     }
 
@@ -33,7 +52,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public void onBindViewHolder(MasonryView masonryView, int position) {
         masonryView.imageView.setImageResource(products.get(position).getImg());
         masonryView.textView.setText(products.get(position).getTitle());
-
+//将数据保存在itemView的Tag中，以便点击时进行获取
+        masonryView.itemView.setTag(products.get(position));
     }
 
     @Override
@@ -41,15 +61,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return products.size();
     }
 
-    public static class MasonryView extends  RecyclerView.ViewHolder{
+    public static class MasonryView extends RecyclerView.ViewHolder {
 
         ImageView imageView;
         TextView textView;
 
-        public MasonryView(View itemView){
+        public MasonryView(View itemView) {
             super(itemView);
-            imageView= (ImageView) itemView.findViewById(R.id.masonry_item_img );
-            textView= (TextView) itemView.findViewById(R.id.masonry_item_title);
+            imageView = (ImageView) itemView.findViewById(R.id.masonry_item_img);
+            textView = (TextView) itemView.findViewById(R.id.masonry_item_title);
         }
 
     }
